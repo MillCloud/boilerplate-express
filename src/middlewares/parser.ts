5e8c650dd6b64256c432857b530dd00b78ef1436
ModyQyW <wurui7@mail3.sysu.edu.cn>
2022-01-07 16:01:43 +0800

    fix: fix typo

--- src/middlewares/parser.ts
+++ src/middlewares/parser.ts
@@ -1,7 +1,7 @@
 import parser from 'body-parser';
 
 export const jsonPaserMiddleware = parser.json();
-export const rawPaserMiddleware = parser.raw();
+export const rawParserMiddleware = parser.raw();
 export const textParserMiddleware = parser.text();
 export const urlencodedParserMiddleware = parser.urlencoded({
   extended: false,
