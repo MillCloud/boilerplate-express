801142da5a236eafb94ca56efe121df952935902
ModyQyW <wurui7@mail3.sysu.edu.cn>
2022-01-11 21:16:04 +0800

    fix: fix logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -6,43 +6,43 @@ import { tracer } from './tracer';
 
 const { format, transports } = winston;
 const { combine, colorize, timestamp, json, errors } = format;
-const { File, Console } = transports;
+const { Console } = transports;
 
 export const logger = winston.createLogger({
   level: isProduction ? 'warn' : 'info',
   transports: isProduction
     ? [
-        new File({
-          filename: path.resolve('logs', 'index.log'),
-          level: 'warn',
-        }),
+        // PERF: consider multi process
         new WinstonDailyRotateFile({
-          filename: 'application-%DATE%.log',
+          filename: '%DATE%.log',
           datePattern: 'YYYY-MM-DD-HH',
           zippedArchive: true,
           dirname: path.resolve('logs'),
           maxFiles: '14d',
+          level: 'warn',
         }),
       ]
     : [new Console()],
   format: combine(
-    colorize(),
-    format((info) => {
-      const id = tracer.id();
-      if (id) {
-        // eslint-disable-next-line no-param-reassign
-        info.requestId = id;
-      }
-      return info;
-    })(),
-    timestamp(),
-    json(),
-    errors({ stack: true }),
-    format.printf(
-      (info) =>
-        `${info?.timestamp} ${info?.requestId ?? ''} ${info?.level}: ${info?.message} ${
-          info?.meta?.res?.statusCode ?? ''
-        } ${info?.meta?.responseTime ?? 0}ms`,
-    ),
+    ...([
+      isProduction ? undefined : colorize(),
+      format((info) => {
+        const id = tracer.id();
+        if (id) {
+          // eslint-disable-next-line no-param-reassign
+          info.requestId = id;
+        }
+        return info;
+      })(),
+      timestamp(),
+      json(),
+      errors({ stack: true }),
+      format.printf(
+        (info) =>
+          `${info?.timestamp} ${info?.requestId ?? ''} ${info?.level}: ${info?.message} ${
+            info?.meta?.res?.statusCode ?? ''
+          } ${info?.meta?.responseTime ?? 0}ms`,
+      ),
+    ].filter((item) => !!item) as winston.Logform.Format[]),
   ),
 });
