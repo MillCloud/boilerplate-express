e14a63ec53d8bcf890d31b5f18080e33ab8561a8
ModyQyW <wurui7@mail3.sysu.edu.cn>
2022-01-30 10:00:05 +0800

    feat: use express-static-gzip

--- src/middlewares/static.ts
+++ src/middlewares/static.ts
@@ -1,11 +1,14 @@
-import serveStatic from 'serve-static';
+import serveStatic from 'express-static-gzip';
 import contentDisposition from 'content-disposition';
 import path from 'path';
 
 export const staticMiddleware = serveStatic(path.resolve('src', 'static'), {
-  fallthrough: false,
   index: false,
-  setHeaders: (response, filePath) => {
-    response.setHeader('Content-Disposition', contentDisposition(filePath));
+  serveStatic: {
+    fallthrough: false,
+    index: false,
+    setHeaders: (response, filePath) => {
+      response.setHeader('Content-Disposition', contentDisposition(filePath));
+    },
   },
 });
