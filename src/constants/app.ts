3aaaf52578c5d537abb2dc70284a08f159342283
ModyQyW <wurui7@mail3.sysu.edu.cn>
2022-01-31 19:40:52 +0800

    feat: add api router constants

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,3 +1,4 @@
+import pkg from '@/../package.json';
 import { NODE_ENV } from './env';
 
 export const APP_MODE = NODE_ENV?.toUpperCase();
@@ -12,4 +13,12 @@ export const APP_PORT = /^\+?[1-9]\d*$/.test(process.env.PORT ?? '')
   ? Number(process.env.APP_PORT)
   : 3000;
 
-export const APP_URL = `${APP_HTTPS ? 'https' : 'http'}://${APP_HOST}:${APP_PORT}`;
+export const APP_API_ROUTER_PREFIX = process.env.APP_API_ROUTER_PREFIX ?? '/api';
+
+export const APP_VERSION = pkg.version;
+
+export const APP_API_ROUTER_VERSION = `v${APP_VERSION.split('.')[0]}`;
+
+export const APP_URL = `${
+  APP_HTTPS ? 'https' : 'http'
+}://${APP_HOST}:${APP_PORT}${APP_API_ROUTER_PREFIX}/${APP_API_ROUTER_VERSION}`;
