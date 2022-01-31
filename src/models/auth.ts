244db509711820eb76b3ce3e4d35cd61d41645af
ModyQyW <wurui7@mail3.sysu.edu.cn>
2022-01-31 19:16:27 +0800

    feat: merge schemas into models

--- src/models/auth.ts
+++ src/models/auth.ts
@@ -1,4 +1,10 @@
 import mongoose from 'mongoose';
-import { authSchema } from '../schemas';
+
+export const authSchema = new mongoose.Schema<IAuth>({
+  token: String,
+  userId: { type: mongoose.Types.ObjectId, required: true },
+  password: { type: String, required: true },
+  expiredAt: { type: Date, required: true },
+});
 
 export const AuthModel = mongoose.model<IAuth>('Auth', authSchema);
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,27 @@
+/* eslint-disable no-param-reassign */
 import mongoose from 'mongoose';
-import { userSchema } from '../schemas';
+
+export const userSchema = new mongoose.Schema<IUser>({
+  username: { type: String, required: true },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+});
+
+userSchema.set('toJSON', {
+  transform: (document, value) => {
+    value.id = value._id.toString();
+    delete value._id;
+    delete value.__v;
+  },
+});
 
 export const UserModel = mongoose.model('User', userSchema);
+/* eslint-enable no-param-reassign */
