4f7ba6c85f8e78ab0132ceb97a3409e4aec0545a
ModyQyW <wurui7@mail3.sysu.edu.cn>
2022-01-31 19:13:45 +0800

    feat: update home routes

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -1,20 +1,18 @@
-import { Router } from '@awaitjs/express';
-import { logger } from '@/utils';
+import { Router } from 'express';
 
+export const base = '/';
 const router = Router();
 
-router.getAsync('/', async (request, response) => {
-  console.log('GET / request.body', request.body);
-  logger.warn('GET WARN Id cillum est mollit reprehenderit enim sint occaecat quis.');
-  logger.error('GET ERROR Id cillum est mollit reprehenderit enim sint occaecat quis.');
-  response.send('Hello Express! This is a GET response.');
+router.get('/', async (request, response) => {
+  response.json({
+    message: 'Hello Express! This is a GET response.',
+  });
 });
 
-router.postAsync('/', async (request, response) => {
-  console.log('POST / request.body', request.body);
-  logger.warn('POST WARN Id cillum est mollit reprehenderit enim sint occaecat quis.');
-  logger.error('POST ERROR Id cillum est mollit reprehenderit enim sint occaecat quis.');
-  response.send('Hello Express! This is a POST response.');
+router.post('/', async (request, response) => {
+  response.json({
+    message: 'Hello Express! This is a POST response.',
+  });
 });
 
 export { router };
