91cd64b027389e5bb0eb1a38adadeda429df51ba
ModyQyW <wurui7@mail3.sysu.edu.cn>
2022-01-31 19:40:24 +0800

    feat: update rate-limit message

--- src/middlewares/rate-limit.ts
+++ src/middlewares/rate-limit.ts
@@ -3,6 +3,9 @@ import rateLimit from 'express-rate-limit';
 export const rateLimitMiddleware = rateLimit({
   windowMs: 1000,
   max: 5,
+  message: {
+    message: 'Too many requests, please try again later.',
+  },
   standardHeaders: true,
   legacyHeaders: false,
 });
