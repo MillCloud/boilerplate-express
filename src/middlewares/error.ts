c2bc105c7ac452c75190667cbc346de4f99205fe
ModyQyW <wurui7@mail3.sysu.edu.cn>
2022-01-31 19:09:01 +0800

    feat: update error middleware

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,10 +1,12 @@
 import { ErrorRequestHandler } from 'express';
+import statuses from 'statuses';
 
 export const errorMiddleware: ErrorRequestHandler = (error, request, response, next) => {
   try {
     const status = error?.status ?? 500;
+    const code = statuses(status);
     const message = error?.message ?? 'Something went wrong.';
-    response.status(status).json({ message });
+    response.status(status).json({ code, message });
   } catch (error_) {
     next(error_);
   }
