588699904812aa740c98a620b4772143b4c816f1
ModyQyW <wurui7@mail3.sysu.edu.cn>
2022-01-31 18:53:00 +0800

    chore: update types

--- src/middlewares/parser.ts
+++ src/middlewares/parser.ts
@@ -1,8 +1,9 @@
 import parser from 'body-parser';
+import { RequestHandler } from 'express';
 
-export const jsonParserMiddleware = parser.json();
-export const rawParserMiddleware = parser.raw();
-export const textParserMiddleware = parser.text();
+export const jsonParserMiddleware = parser.json() as RequestHandler;
+export const rawParserMiddleware = parser.raw() as RequestHandler;
+export const textParserMiddleware = parser.text() as RequestHandler;
 export const urlencodedParserMiddleware = parser.urlencoded({
   extended: false,
-});
+}) as RequestHandler;
