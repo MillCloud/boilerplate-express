4b5ff61bf3fafb5cd2ac03924d2d7ff4afc2bcfa
ModyQyW <wurui7@mail3.sysu.edu.cn>
2022-01-31 19:41:34 +0800

    feat: add rateLimitMiddleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,16 @@
+import { APP_API_ROUTER_PREFIX, APP_API_ROUTER_VERSION } from '@/constants';
+import { rateLimitMiddleware } from '@/middlewares';
 import { Express } from 'express';
 import { router as authRouter, base as authRouterBase } from './auth';
 import { router as homeRouter, base as homeRouterBase } from './home';
 
+const path = (routerBase: string) =>
+  `${APP_API_ROUTER_PREFIX}/${APP_API_ROUTER_VERSION}/${routerBase}`.replace(/\/+/g, '/');
+
 export default (app: Express) => {
-  app.use(authRouterBase, authRouter);
-  app.use(homeRouterBase, homeRouter);
+  app.use(APP_API_ROUTER_PREFIX, rateLimitMiddleware);
+  app.use(path(authRouterBase), authRouter);
+  app.use(path(homeRouterBase), homeRouter);
   app.use('*', (request, response, next) => {
     next({
       status: 404,
