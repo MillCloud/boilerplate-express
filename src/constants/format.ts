7337ff338ddb3e4c738d954b4d227382c2899670
ModyQyW <wurui7@mail3.sysu.edu.cn>
2022-01-31 18:45:25 +0800

    feat: add format constants

---
+++ src/constants/format.ts
@@ -0,0 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export const ISO8601_Format = undefined;
+
+export const DATE_FORMAT = 'YYYY-MM-DD';
+
+export const DATE_TIME_FORMAT = 'YYYY-MM-DD HH:mm:ss';
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,5 @@
 export * from './app';
 export * from './db';
 export * from './env';
+export * from './format';
 export * from './logger';
