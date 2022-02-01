433ea3bdecbbe4bf79cb069e621b341c93c6a85b
ModyQyW <wurui7@mail3.sysu.edu.cn>
2022-02-01 11:18:37 +0800

    feat: update typings

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -1,6 +1,11 @@
 import mongoose from 'mongoose';
 
 declare global {
+  type TDocument<T = any> = mongoose.Document<unknown, any, T> &
+    T & {
+      _id: mongoose.Types.ObjectId;
+    };
+
   interface IAuth {
     token: string;
     userId: mongoose.Types.ObjectId;
@@ -8,11 +13,16 @@ declare global {
     expiredAt: Date;
   }
 
+  interface IAuthDocument extends TDocument<IAuth> {}
+
   interface IUser {
     username: string;
+    role: number;
     createdAt?: Date;
     updatedAt?: Date;
   }
+
+  interface IUserDocument extends TDocument<IUser> {}
 
   interface Body {
     user: mongoose.Document<unknown, any, IUser> &
