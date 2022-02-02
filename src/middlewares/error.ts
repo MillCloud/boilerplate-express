40f3c5253c49369cf3aa504e6c5bb375e0d5df0f
ModyQyW <wurui7@mail3.sysu.edu.cn>
2022-02-02 22:40:55 +0800

    perf: allow error additional message and override option

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -3,10 +3,16 @@ import statuses from 'statuses';
 
 export const errorMiddleware: ErrorRequestHandler = (error, request, response, next) => {
   try {
-    const status = error?.status ?? 500;
-    const code = statuses(status);
-    const message = error?.message ?? 'Something went wrong.';
-    response.status(status).json({ code, message });
+    const errorStatus = error?.status ?? 500;
+    const errorCode = statuses(errorStatus);
+    const errorMessage = (error?.message ?? 'Something went wrong.').trim();
+    const errorOverride = error?.override ?? false;
+
+    const message = (errorOverride ? errorMessage : `${errorCode}. ${errorMessage}.`).replace(
+      /\.+/g,
+      '.',
+    );
+    response.status(errorStatus).json({ code: errorCode, message });
   } catch (error_) {
     next(error_);
   }
