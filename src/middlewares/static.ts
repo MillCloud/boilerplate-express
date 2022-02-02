84f3f603f3e84f9b54fe2f3ddea0bebf3fe2e403
ModyQyW <wurui7@mail3.sysu.edu.cn>
2022-02-02 23:26:30 +0800

    feat: move to serve-static

--- src/middlewares/static.ts
+++ src/middlewares/static.ts
@@ -1,14 +1,11 @@
-import serveStatic from 'express-static-gzip';
+import serveStatic from 'serve-static';
 import contentDisposition from 'content-disposition';
 import path from 'path';
 
 export const staticMiddleware = serveStatic(path.resolve(process.cwd(), 'static'), {
   index: false,
-  serveStatic: {
-    fallthrough: false,
-    index: false,
-    setHeaders: (response, filePath) => {
-      response.setHeader('Content-Disposition', contentDisposition(filePath));
-    },
+  fallthrough: false,
+  setHeaders: (response, filePath) => {
+    response.setHeader('Content-Disposition', contentDisposition(filePath));
   },
 });
